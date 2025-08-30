648d27c40d7180f095ce345c4dffd8241c4978df
Loqrp <loqrpreal@gmail.com>
2025-08-30 10:58:51 +0300

    Update universal.lua

--- games/universal.lua
+++ games/universal.lua
@@ -260,6 +260,15 @@ vape.Libraries.auraanims = {
 		{CFrame = CFrame.Angles(math.rad(90), 0, math.rad(15)), Time = 0.12},
 		{CFrame = CFrame.Angles(0, 0, math.rad(15)), Time = 0.12}
 	},
+	Radium = {
+		{CFrame = CFrame.new(-0.17, 0.14, -0.12) * CFrame.Angles(math.rad(-53), math.rad(50), math.rad(-64)), Time = 0.1},
+		{CFrame = CFrame.new(-0.55, 0.12, -0.1) * CFrame.Angles(math.rad(-161), math.rad(54), math.rad(-6)), Time = 0.08},
+		{CFrame = CFrame.new(-0.62, 0.10, -0.07) * CFrame.Angles(math.rad(-167), math.rad(47), math.rad(-1)), Time = 0.03},
+		{CFrame = CFrame.new(-0.56, 0.12, 0.23) * CFrame.Angles(math.rad(-167), math.rad(49), math.rad(-1)), Time = 0.03}
+	},
+	StaticBlock = {
+		{CFrame = CFrame.new(0.69, -0.7, 0.6) * CFrame.Angles(math.rad(-30), math.rad(50), math.rad(-90)), Time = 0.15},
+	},
 	Exhibition = {
 		{CFrame = CFrame.new(0.69, -0.7, 0.6) * CFrame.Angles(math.rad(-30), math.rad(50), math.rad(-90)), Time = 0.1},
 		{CFrame = CFrame.new(0.7, -0.71, 0.59) * CFrame.Angles(math.rad(-84), math.rad(50), math.rad(-38)), Time = 0.2}
